24dda54258be417c519c996e75ad5371306d9d06
hawk198723 <hawk198723@gmail.com>
2022-02-10 01:56:19 -0800

    nav done --> mess up with header and App.js, later will straight up all the routes on the homepage

---
+++ app/javascript/components/components/Header.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class Header extends Component {
+	render() {
+		const { logged_in, current_user, new_user_route, sign_in_route, sign_out_route } = this.props;
+		console.log('logged_in:', logged_in);
+		console.log('current_user:', current_user);
+		console.log('new_user_route:', new_user_route);
+		console.log('sign_in_route:', sign_in_route);
+		console.log('sign_out_route:', sign_out_route);
+		return (
+			<nav>
+				<h2>Header</h2>
+				<ul>
+					<li>
+						<NavLink to="/">Home</NavLink>
+					</li>
+					<li>
+						<NavLink to="/apartmentsindex">Apartments</NavLink>
+					</li>
+
+					{!logged_in && (
+						<li>
+							<a href={new_user_route}>Register</a>
+						</li>
+					)}
+					{!logged_in && (
+						<li>
+							<a href={sign_in_route}>Sign In</a>
+						</li>
+					)}
+					{logged_in && (
+						<li>
+							<a href={sign_out_route}>Sign Out</a>
+						</li>
+					)}
+				</ul>
+			</nav>
+		);
+	}
+}
+export default Header;
