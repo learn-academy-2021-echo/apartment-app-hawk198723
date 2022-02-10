a95269187194a5dcd6c94b9eace23a112f73fa04
hawk198723 <hawk198723@gmail.com>
2022-02-09 18:54:10 -0800

    installs-resources done

---
+++ app/javascript/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		const { logged_in, current_user, new_user_route, sign_in_route, sign_out_route } = this.props;
+		return <h1>Hi</h1>;
+	}
+}
+
+export default App;
